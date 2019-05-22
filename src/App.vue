8ea96325177fdcf6895c6823c907b882d3c8e4aa
wangjialu <729971559@qq.com>
2019-05-22 17:41:56 +0800

    5-22更新

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mt-header fixed title="信息管理系统"></mt-header>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="首页">
+        <img slot="icon" src="./assets/首页.png" alt="">首页
+      </mt-tab-item>
+      <mt-tab-item id="会员">
+        <img slot="icon" src="./assets/会员.png" alt="">会员
+      </mt-tab-item>
+      <mt-tab-item id="购物车">
+        <img slot="icon" src="./assets/购物车空.png" alt="">购物车
+      </mt-tab-item>
+      <mt-tab-item id="查找">
+        <img slot="icon" src="./assets/查找.png" alt="">查找
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      selected: ''
+    }
+  },
+  watch:{
+    selected(newVal,oldVal){
+      console.log(newVal,oldVal)
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 配置MintUI
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+// 安装插件 注册全局组件及挂载属性
+Vue.use(MintUI)
+
+// 配置axios
+import Axios from 'axios'
+Axios.defaults.baseURL = 'https://www.sinya.online/api/'
+Vue.prototype.$axios = Axios
+
+// 切换开发和生产模式的提示
 Vue.config.productionTip = false
 
 new Vue({
